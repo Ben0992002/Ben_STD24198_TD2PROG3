37b3b8874a9c582195b3d183cdba163e992ad5b3
Issa Ben Saïd <hei.issa.29@gmail.com>
2026-01-18 16:23:53 +0300

    Ajout de l'attribut goalNb et des getters/setters dans Player

---
+++ src/main/java/org/example/Player.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class Player {
+    private Integer id;
+    private String name;
+    private Integer age;
+    private Integer goalNb;
+    private PlayerPositionEnum position;
+    private Team team;
+
+    public Player() {}
+
+    // Getters et Setters
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public Integer getAge() { return age; }
+    public void setAge(Integer age) { this.age = age; }
+    public Integer getGoalNb() { return goalNb; }
+    public void setGoalNb(Integer goalNb) { this.goalNb = goalNb; }
+    public PlayerPositionEnum getPosition() { return position; }
+    public void setPosition(PlayerPositionEnum position) { this.position = position; }
+    public Team getTeam() { return team; }
+    public void setTeam(Team team) { this.team = team; }
+}
