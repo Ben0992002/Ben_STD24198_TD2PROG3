567de888f426a20f619eed411d81b0782f398d75
Issa Ben Saïd <hei.issa.29@gmail.com>
2026-01-21 18:37:31 +0300

    Nettoyage final exam : suppression attributs TD ajouté par erreur

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -1,26 +1,19 @@
 package org.example;
 
 public class Player {
-    private Integer id;
-    private String name;
-    private Integer age;
-    private Integer goalNb;
-    private PlayerPositionEnum position;
-    private Team team;
+    public int id;
+    public String name;
+    public int age;
+    public PlayerPositionEnum position;
+    public Team team;
+    public Integer goalNb;
 
-    public Player() {}
-
-    // Getters et Setters
-    public Integer getId() { return id; }
-    public void setId(Integer id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public Integer getAge() { return age; }
-    public void setAge(Integer age) { this.age = age; }
-    public Integer getGoalNb() { return goalNb; }
-    public void setGoalNb(Integer goalNb) { this.goalNb = goalNb; }
-    public PlayerPositionEnum getPosition() { return position; }
-    public void setPosition(PlayerPositionEnum position) { this.position = position; }
-    public Team getTeam() { return team; }
-    public void setTeam(Team team) { this.team = team; }
-    
+    public Player(int id, String name, int age, PlayerPositionEnum position, Team team, Integer goalNb) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.position = position;
+        this.team = team;
+        this.goalNb = goalNb;
+    }
+}
